a25d27ced7d95dc9cfc57ed189fded11c608edfc
zuTTers <zubeyir.kocalioglu@gmail.com>
2018-07-06 23:45:35 +0300

    Logs tablosundan son 10 kayıt mail olarak alma methodu eklendi.

--- KTOtomasyon/Controllers/LoginController.cs
+++ KTOtomasyon/Controllers/LoginController.cs
@@ -33,10 +33,8 @@ namespace KTOtomasyon.Controllers
                 {
                     Session["UserId"] = userLogin.User_Id;
                     Session["Name"] = userLogin.Name;
-                    //NowSendMail();
-                    SendMail();
-                    //Shared.LoginSendMail();
-                    //Shared.DefaultSendMail();
+                    SendOrderMail();
+                    SendLogMail();
                     return RedirectToAction("Index", "Home");
                 }
                 else
@@ -84,56 +82,7 @@ namespace KTOtomasyon.Controllers
             return View();
         }
 
-        //Body ve subject mail gönderir
-        public ActionResult NowSendMail()
-        {
-            try
-            {
-                SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com", 587); //587
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtp.EnableSsl = true;
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new System.Net.NetworkCredential("simpleterzi3428@outlook.com", "3428simple");
-
-                MailMessage mail = new MailMessage();
-                mail.From = new MailAddress("simpleterzi3428@outlook.com", "Simple Terzi - Axis");
-                mail.To.Add(new MailAddress("simpleterzi3428@outlook.com"));
-                mail.Bcc.Add(new MailAddress("zubeyir.kocalioglu@gmail.com", "Zübeyir KOÇALİOĞLU"));
-
-                //Mails addmail = new Mails();
-
-                using (var db = new KTOtomasyonEntities())
-                {
-                    var mymaildata = db.vLastTotalOrder.OrderByDescending(x => x.Sira).First();
-                    var mylogdata = db.Logs.OrderByDescending(x => x.Log_Id).First();
-
-                    if (mylogdata != null && mymaildata != null)
-                    {
-                        mail.Subject = "Simple Terzi Sipariş Rapor";
-                        mail.Body = "Bugün : Toplam sipariş miktarı '";
-                        mail.Body += mymaildata.SipMiktar.Value + "' ve sipariş tutarı '" + mymaildata.SipTutar.Value + "'₺ dir.";
-                        mail.Body += "Hata mesajı method adı :  '" + mylogdata.MethodName + "' ve Exception mesajı : '" + mylogdata.ExMessage;
-                    }
-                    else
-                    {
-                        mail.Subject = "Simple Terzi Sipariş Rapor";
-                        mail.Body = "Bugün, sipariş yok. '";
-                        mail.Body += "Hata mesajı method adı :  '" + mylogdata.MethodName + "' ve Exception mesajı : '" + mylogdata.ExMessage;
-                    }
-
-                }
-                smtp.Send(mail);
-
-            }
-            catch (Exception ex)
-            {
-                ex.AddToDBLog("NowSendMail", ex.Message);
-            }
-
-            return RedirectToAction("Index", "Home");
-        }
-
-        public ActionResult SendMail()
+        public ActionResult SendOrderMail()
         {
             SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com", 587);
             smtp.UseDefaultCredentials = false;
@@ -147,7 +96,7 @@ namespace KTOtomasyon.Controllers
             string body = "";
             String ConnStr = "Data Source=ZUTTERS;Initial Catalog=KTOtomasyon;User ID=zubeyir;Password=123456789;";
             String SQL = "SELECT Sira, SipMiktar, SipTutar FROM vLastTotalOrder "
-               + "WHERE Sira IS NOT NULL";
+                       + "WHERE Sira IS NOT NULL";
             SqlDataAdapter TitlesAdpt = new SqlDataAdapter(SQL, ConnStr);
             DataSet Titles = new DataSet();
             try
@@ -155,7 +104,7 @@ namespace KTOtomasyon.Controllers
                 TitlesAdpt.Fill(Titles);
                 
                 //Header
-                body = "<h3 align='center'>Simple Terzi Haftanın Raporu</h3>";
+                body = "<h3 align='center'>Simple Terzi - Haftanın Sipariş Raporu</h3>";
 
                 //Content
                 body += "<table width='70%' bgcolor='#f6f8f1' border='0' cellpadding='0' cellspacing='0' class='table table-striped text-center' align='center'>";
@@ -188,7 +137,7 @@ namespace KTOtomasyon.Controllers
                 mail.From = new MailAddress("simpleterzi3428@outlook.com", "Simple Terzi - Axis");
                 mail.To.Add(new MailAddress("simpleterzi3428@outlook.com"));
                 mail.Bcc.Add(new MailAddress("zubeyir.kocalioglu@gmail.com", "Zübeyir KOÇALİOĞLU"));
-                mail.Subject = "Simple Terzi Rapor";
+                mail.Subject = "Simple Terzi Rapor - Sipariş Raporu";
                 mail.Body = body;
                 mail.IsBodyHtml = true;
 
@@ -202,7 +151,84 @@ namespace KTOtomasyon.Controllers
             }
             catch (Exception ex)
             {
-                ex.AddToDBLog("SendMail", ex.Message);
+                ex.AddToDBLog("SendOrderMail", ex.Message);
+            }
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        public ActionResult SendLogMail()
+        {
+            SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential("simpleterzi3428@outlook.com", "3428simple");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = true;
+
+            //smtp.Timeout = 600000;
+
+
+            string body = "";
+            String ConnStr = "Data Source=ZUTTERS;Initial Catalog=KTOtomasyon;User ID=zubeyir;Password=123456789;";
+            String SQL = "SELECT TOP 10 Log_Id, MethodName, Message, CreatedDate FROM Logs WHERE Log_Id IS NOT NULL Order by Log_Id desc";
+            SqlDataAdapter TitlesAdpt = new SqlDataAdapter(SQL, ConnStr);
+            DataSet Titles = new DataSet();
+            try
+            {
+                TitlesAdpt.Fill(Titles);
+
+                //Header
+                body = "<h3 align='center'>Simple Terzi - Son 10 Hata Raporu</h3>";
+
+                //Content
+                body += "<table width='70%' bgcolor='#f6f8f1' border='0' cellpadding='0' cellspacing='0' class='table table-striped text-center' align='center'>";
+                body += "<tr bgcolor='#c7d8a7'>";
+                body += "<td><strong>Log ID<strong></td>";
+                body += "<td><strong>Metod Adı<strong></td>";
+                body += "<td><strong>Hata Mesajı<strong></td>";
+                body += "<td><strong>Oluşma Tarihi<strong></td>";
+                body += "</tr>";
+                foreach (DataRow Title in Titles.Tables[0].Rows)
+                {
+                    body += "<tr>";
+                    body += "<td>" + Title[0] + "</td>";
+                    body += "<td>" + Title[1] + "</td>";
+                    body += "<td>" + Title[2] + "</td>";
+                    body += "<td>" + String.Format("{0:d/M/yyyy HH:mm:ss}", Title[3]) + "</td>";
+                    body += "</tr>";
+                }
+                body += "</table>";
+
+                //Footer
+                body += "</hr>";
+                body += "<h5 align='center'>";
+                body += "zuTTers, İstanbul 2018 ";
+                body += "<a href='twitter.com/zkocalioglu'><font color='red'> İletişim </font></a> | Zübeyir Koçalioğlu tarafından gönderildi.";
+                body += "</h5>";
+
+
+
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("simpleterzi3428@outlook.com", "Simple Terzi - Axis");
+                mail.To.Add(new MailAddress("simpleterzi3428@outlook.com"));
+                mail.Bcc.Add(new MailAddress("zubeyir.kocalioglu@gmail.com", "Zübeyir KOÇALİOĞLU"));
+                mail.Subject = "Simple Terzi Rapor - Hata Raporu";
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+
+                ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+
+                Shared.AddToDBMail(mail.Body, mail.Subject, mail.From.ToString(), mail.To.ToString());
+
+                smtp.Send(mail);
+
+
+            }
+            catch (Exception ex)
+            {
+                ex.AddToDBLog("SendLogMail", ex.Message);
             }
 
             return RedirectToAction("Index", "Home");
