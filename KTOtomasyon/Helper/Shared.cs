5ea8d48507519e50e805371596677955f73bc353
Zübeyir Koçalioğlu <zubeyir.kocalioglu@gmail.com>
2018-07-07 20:10:28 +0300

    Update Shared.cs

--- KTOtomasyon/Helper/Shared.cs
+++ KTOtomasyon/Helper/Shared.cs
@@ -127,7 +127,7 @@ namespace KTOtomasyon.Controllers
             smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtp.EnableSsl = true;
             smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new System.Net.NetworkCredential("simpleterzi3428@outlook.com", "3428simple");
+            smtp.Credentials = new System.Net.NetworkCredential("simpleterzi3428@outlook.com", "pass");
 
             try
             {
@@ -170,7 +170,7 @@ namespace KTOtomasyon.Controllers
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
                 smtp.EnableSsl = true;
                 smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new System.Net.NetworkCredential("simpleterzi3428@outlook.com", "3428simple");
+                smtp.Credentials = new System.Net.NetworkCredential("simpleterzi3428@outlook.com", "pass");
 
                 MailMessage mail = new MailMessage();
                 mail.From = new MailAddress("simpleterzi3428@outlook.com", "Simple Terzi - Axis");
